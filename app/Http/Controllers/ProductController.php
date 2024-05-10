396707fc62c0f099dc4a8e00f73b2e7a06f3b542
Mikeeeyy <mirempillo@my.cspc.edu.ph>
2024-05-09 22:53:33 -0700

    Mike

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = "Product list form in Product Controller";
+        return view('products.index', ['products' => $products]);
+    }
+
+    public function create (){
+        return view('products.create');
+    }
+}
